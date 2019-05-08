0118b728d8ab81f79b29e31ca2b172f1667e9f84
Dharti19 <dhartirai@gmail.com>
2019-05-08 11:53:33 +0530

    AddedSecond branch

---
+++ src/org/test/projectone/Ignore_Duplicate_SecondBr.java
@@ -0,0 +1,12 @@
+package org.test.projectone;
+
+public class Ignore_Duplicate_SecondBr {
+
+	
+	
+public static void main(String[] args) {
+System.out.println("Dharti Rai");	
+}
+		// TODO Auto-generated method stub
+
+	}
